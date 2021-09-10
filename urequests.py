2f3511e0fe39fbe93fb0fbe6f46038de0c270fdf
Yanni Pang <yanni@bu.edu>
2021-09-10 16:40:13 -0400

    added s.close() to close connection

--- urequests.py
+++ urequests.py
@@ -105,6 +105,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
     resp = Response(s)
     resp.status_code = status
     resp.reason = reason
+    s.close()
     return resp
 
 
