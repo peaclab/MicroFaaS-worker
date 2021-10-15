379c399108f6e6ddb26e33ee162108b43c815179
allenz1120 <55994268+allenz1120@users.noreply.github.com>
2021-10-15 14:39:27 -0400

    Changed microfaas directory to microfaas

--- start_worker.sh
+++ start_worker.sh
@@ -5,8 +5,8 @@ echo 1 > /proc/sys/kernel/sysrq
 
 # Start worker, with output to syslog
 echo "in start worker and about to run worker.py"
-cd /root/MicroFaaS
-/sbin/micropython /root/MicroFaaS/worker.py > /dev/ttyS0
+cd /root/MicroFaaS-worker
+/sbin/micropython /root/MicroFaaS-worker/worker.py > /dev/ttyS0
 # | logger -s -p 3
 
 # Failsafe Reboot (if worker.py doesn't do this for us)
